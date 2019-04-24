80df48188514dbd5b1e78810d66fcaf9517a507e
Mike Kovacina <mike.kovacina@hyland.com>
2019-04-24 10:45:22 -0400

    fixed most errors; still figuring out generate_n

--- ga-tbontb/main.cpp
+++ ga-tbontb/main.cpp
@@ -2,63 +2,82 @@
 #include <algorithm>
 #include <iterator>
 #include <list>
+#include <string>
 
-typedef struct Parameters
+namespace tbontb
 {
-    unsigned int PopulationSize;
-    float MutationRate;
-    float CrossoverRate;
-    unsigned int MemberLength;
-};
+	using namespace std;
 
-// couldnt keep things simple
-// wanted to have levels and timestamps
-namespace log
-{
-    void info(const string& message)
-    {
-        std::clog << message;
-    }
-    
-    void trace(const string& message)
-    {
-        std::clog << message;
-    }
+	struct Parameters
+	{
+		unsigned int PopulationSize;
+		float MutationRate;
+		float CrossoverRate;
+		unsigned int MemberLength;
+	};
+
+	// couldnt keep things simple
+	// wanted to have levels and timestamps
+	namespace log
+	{
+		void info(const string& message)
+		{
+			clog << message;
+		}
+
+		void trace(const string& message)
+		{
+			clog << message;
+		}
+	}
+
+	std::string GenerateRandomMember(unsigned int length)
+	{
+		auto member = string(length,' ');
+		auto lambda = [=](){ return 'a';};
+		std::generate_n(back_inserter(member), length, lambda);
+		return member;
+	}
+
+	void go()
+	{
+		auto parameters = Parameters {100,.5,.5};
+
+		// this was initially a char[][]
+		// then a string[] once inswitched
+		//     to c++ becuase i amm not a complete savage
+		// then a std::vector as a got my groove going
+		// then a std::list since im going to be doing 
+		//    a lot of manipulation
+		// but in the end inwant tonmove back to 
+		//    something constant without continued 
+		//    allocation
+		std::list<string> population;
+
+		log::info("initializing population");
+
+		// i was going to use a for-loop
+		// but why when i have the stl
+		//auto* iterator = back_inserter(population);
+		/*
+		std::generate_n(
+				//iterator, 
+				back_inserter(population),
+				parameters.PopulationSize, 
+				[=](){GenerateRandomMember(parameters.MemberLength);});
+		*/
+
+
+		do
+		{
+
+		}
+		while(true);
+	}
 }
 
-string GenerateRandomMember(unsigned int length)
-{
-    auto member = string(length);
-    auto lambda = [](){ return 'a';};
-    std::generator(member, length, lambda);
-    return member;
-}
 
 int main(int argc, char** argv)
 {
-    auto parameters = {100,.5,.5};
-    
-    // this was initially a char[][]
-    // then a string[] once inswitched
-    //     to c++ becuase i amm not a complete savage
-    // then a std::vector as a got my groove going
-    // then a std::list since im going to be doing 
-    //    a lot of manipulation
-    // but in the end inwant tonmove back to 
-    //    something constant without continued 
-    //    allocation
-    std::list<string> population;
-    
-    log::info("initializing population");
-    
-    // i was going to use a for-loop
-    // but why when i have the stl
-    auto* iterator = back_inserter(population);
-    std::generate(iterator, parameters.PopulationSize, [=](){GenerateRandomMember(parameters.MemberLength);});
-     
-    do
-    {
-        
-    }
-    while(true);
+	tbontb::go();
 }
