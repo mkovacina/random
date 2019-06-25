f3b7ebf9bdd1945f2943d1bead79c533d770a941
Mike Kovacina <mike.kovacina@hyland.com>
2019-06-25 10:24:42 -0400

    sample code to add a key to a json object when only the partial path exists

---
+++ json-manipulation/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+
+namespace json_manipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = File.ReadAllText("sample.json");
+            var json = JObject.Parse(data);
+            Console.WriteLine(json);
+
+            var paths = "a:b:c:d:e:f".Split(':');
+
+            var xp = "";
+            int x = 0;
+            for(x = 0; x < paths.Length - 1; x++)
+            {
+                var path = paths[x];
+
+                var token = json.SelectToken(xp+"."+path);
+                if (token == null)
+                {
+                    Console.WriteLine($"'{xp}' not found.");
+                    break;
+                }
+
+                if (xp == string.Empty)
+                {
+                    xp = path;
+                }
+                else
+                {
+                    xp += "."+path;
+                }
+                Console.WriteLine($"'{xp}' found.");
+            }
+
+                var token2 = json.SelectToken(xp);
+            for(; x < paths.Length-1;x++)
+            {
+               token2[paths[x]]=new JObject();
+               xp += "." + paths[x];
+               token2 = token2[paths[x]];
+            }
+
+            token2[paths.Last()]="encrypted string";
+            Console.WriteLine(json.ToString());
+        }
+    }
+}
